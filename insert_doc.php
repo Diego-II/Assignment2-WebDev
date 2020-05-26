3d6aa29ec21045388628f4db5a36beda5cd26d15
Diego-II <43184602+dirarra@users.noreply.github.com>
2020-05-26 17:23:54 -0400

    Agregar doctor OK!

--- insert_doc.php
+++ insert_doc.php
@@ -8,33 +8,85 @@ if (!file_exists($cd . "/media")){
     mkdir($cd . "/media", "0777");
 }
 
+/**NON file data: */
+$nombreMedico = $_POST["nombre-medico"];
+$regionMedico = $_POST["region-medico"];
+$comunaMedico = $_POST["comuna-medico"];
+$celMedico = $_POST["celular-medico"];
+$mailMedico = $_POST["email-medico"];
+$twitMedico = $_POST["twitter-medico"];
+$expMedico = $_POST["experiencia-medico"];
+
+/**Create array with 'especialidades' */
+$especialidadesMedico = array();
+array_push($especialidadesMedico, $_POST["especialidades-medico"],$_POST["especialidades-medico-2"],
+$_POST["especialidades-medico-3"],$_POST["especialidades-medico-4"],$_POST["especialidades-medico-5"]);
+/**Delete the 'especialidades' left blanc */
+$especialidadesMedico = \array_diff($especialidadesMedico,["sin-especialidad"]);
+
+/**Get files: */
+$target_dir = "media/";
+
+$allowed_image_extension = array("jpg","png","jpeg", "gif");
+
+$fotos = array();
+$msg = array();
+$uploadOkArray = array();
+foreach($_FILES as $file){
+    if ($file["name"] == NULL){
+    break;
+    }
+    $target_file = $target_dir . basename($file["name"]);
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    array_push($fotos,basename($file["name"]));
+    $uploadOk = 1;
+    if(isset($_POST["submitDoc"])){
+        $check = getimagesize($file["tmp_name"]);
+        if($check == false){
+            $msg[] =  "Archivo ". basename( $file["name"]). " no es una imagen.";
+            $uploadOk = 0;
+        }else{
+            $uploadOk = 1;
+        }
+    }
+    if(file_exists($target_file)){
+        $msg[] = "Imagen ". basename( $file["name"]). " ya existe";
+        $uploadOk = 0;
+    }
+    if (!in_array($imageFileType,$allowed_image_extension)) {
+        $msg[] = "Formato de la imagen ". basename( $file["name"]). " no permitido.";
+        $uploadOk = 0;
+    }
+    if ($file["size"] > 5000000) {
+        $msg[] = "Imagen ". basename( $file["name"]). " excede tamano permitido (5 [mb]).";
+        $uploadOk = 0;
+    }
+
+    if(!$uploadOk){
+        $msg[] = "Imagen ". basename( $file["name"]). " no fue subida.";
+        $uploadOkArray[] = 0;
+    } else{
+        if (move_uploaded_file($file["tmp_name"], $target_file)){
+            $msg[] = "Imagen ". basename( $file["name"]). " subida correctamente.";
+            $uploadOkArray[] = 1;
+        } else{
+            $msg[] = "Imagen ". basename( $file["name"]). " no fue subida.";
+            $uploadOkArray[] = 0;
+        }
+    }
+}
 
 $db = DbConfig::getConnection();
 if(isset($_POST["submitDoc"])){
 
-    $nombreMedico = $_POST["nombre-medico"];
-    $regionMedico = $_POST["region-medico"];
-    $comunaMedico = $_POST["comuna-medico"];
-    $celMedico = $_POST["celular-medico"];
-    $mailMedico = $_POST["email-medico"];
-    $twitMedico = $_POST["twitter-medico"];
-    $expMedico = $_POST["experiencia-medico"];
-    
-    /**Create array with 'especialidades' */
-    $especialidadesMedico = array();
-    array_push($especialidadesMedico, $_POST["especialidades-medico"],$_POST["especialidades-medico-2"],
-    $_POST["especialidades-medico-3"],$_POST["especialidades-medico-4"],$_POST["especialidades-medico-5"]);
-    /**Delete the 'especialidades' left blanc */
-    $especialidadesMedico = \array_diff($especialidadesMedico,["sin-especialidad"]);
-
-
     insertDoc($db, $nombreMedico, $regionMedico, $comunaMedico, $celMedico, 
-    $mailMedico, $twitMedico, $expMedico, $especialidadesMedico);
+    $mailMedico, $twitMedico, $expMedico, $especialidadesMedico, $msg, $uploadOkArray, $fotos, $target_dir);
+
 }
 $db->close();
 
 
-function insertDoc($db, $nombre,$region, $comuna, $cel, $mail, $twit, $exp, $esp){
+function insertDoc($db, $nombre,$region, $comuna, $cel, $mail, $twit, $exp, $esp, $msg, $okArray, $fotos, $target_dir){
     $encontrar_comuna="SELECT id FROM comuna WHERE nombre LIKE '$comuna'";
     $resultado = $db->query($encontrar_comuna);
     $id_comuna = mysqli_fetch_array($resultado)["id"];
@@ -55,5 +107,21 @@ function insertDoc($db, $nombre,$region, $comuna, $cel, $mail, $twit, $exp, $esp
             $sql2->execute();
         }
     }
+
+    foreach($fotos as $key => $value){
+        //Insertamos solo si se cargo en el directorio media. 
+        if($okArray[$key]){
+            $sql3=$db->prepare("INSERT INTO foto_medico (ruta_archivo,nombre_archivo,medico_id) VALUES ('$target_dir','$value','$id_medico')");
+		    $sql3->execute();
+        }
+    }
+
+
+    echo "<h1>Registro de medico completado.</h1><h2>Redireccionando a pagina inicial.</h2>";
+
+    foreach($msg as $key => $value){
+        echo $value, '<br>';
+    }
+    echo "<td><button onclick = \"location.href = 'Inicio.html';\">Volver a pagina inicial</button></td>";
 }
 ?>
