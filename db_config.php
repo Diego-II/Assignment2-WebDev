a7dea4a24c67c9201cf1f671741d3ec143cf65f3
Diego-II <43184602+dirarra@users.noreply.github.com>
2020-05-26 11:59:09 -0400

    Conexion con cc500217_db funciona. Viaja info de los select e input's. Faltan files.

--- db_config.php
+++ db_config.php
@@ -1,6 +1,6 @@
 <?php
 class DbConfig{
-	private static $db_name = "cc5002_db"; //Base de datos de la app
+	private static $db_name = "cc500217_db"; //Base de datos de la app
 	private static $db_user = "usuario"; //Usuario MySQL
 	private static $db_pass = "cc5002"; //Password
 	private static $db_host = "localhost";//Servidor donde esta alojado, puede ser 'localhost' o una IP (externa o interna).
@@ -8,7 +8,7 @@ class DbConfig{
 	public static function getConnection(){
 		$mysqli = new mysqli(self::$db_host, self::$db_user, self::$db_pass, self::$db_name);
 
-		$mysqli->set_charset("utf8"); //Muy importante!!
+		$mysqli->set_charset("utf8");
 		return $mysqli;
 	}
 }
--- insert_doc.php
+++ insert_doc.php
@@ -9,35 +9,51 @@ if (!file_exists($cd . "/media")){
 }
 
 
-$nombreMedico = $_POST['nombre-medico'];
-$regionMedico = $_POST["region-medico"];
-$comunaMedico = $_POST["comuna-medico"];
-$celMedico = $_POST["celular-medico"];
-$mailMedico = $_POST["email-medico"];
-$twitMedico = $_POST["twitter-medico"];
-$expMedico = $_POST["experiencia-medico"];
-
-/**Create array with 'especialidades' */
-$especialidadesMedico = array();
-array_push($especialidadesMedico, $_POST["especialidades-medico"],$_POST["especialidades-medico-2"],
-$_POST["especialidades-medico-3"],$_POST["especialidades-medico-4"],$_POST["especialidades-medico-5"]);
-/**Delete the 'especialidades' left blanc */
-$especialidadesMedico = \array_diff($especialidadesMedico,["sin-especialidad"]);
-
 $db = DbConfig::getConnection();
 if(isset($_POST["submitDoc"])){
-    $encontrar_comuna="SELECT id FROM comuna WHERE nombre LIKE '$comunaMedico'";
-    $resultado = $db->query($encontrar_comuna);
-    $id_comuna = mysqli_fetch_array($resultado)["id"];
-            
-    $sql=$db->prepare("INSERT INTO medico (nombre, experiencia, comuna_id, twitter, email, celular) 
-    VALUES ('$nombreMedico', '$expMedico', '$id_comuna', '$twitMedico', '$mailMedico', '$celMedico') ");
 
-    $sql->execute();
+    $nombreMedico = $_POST["nombre-medico"];
+    $regionMedico = $_POST["region-medico"];
+    $comunaMedico = $_POST["comuna-medico"];
+    $celMedico = $_POST["celular-medico"];
+    $mailMedico = $_POST["email-medico"];
+    $twitMedico = $_POST["twitter-medico"];
+    $expMedico = $_POST["experiencia-medico"];
+    
+    /**Create array with 'especialidades' */
+    $especialidadesMedico = array();
+    array_push($especialidadesMedico, $_POST["especialidades-medico"],$_POST["especialidades-medico-2"],
+    $_POST["especialidades-medico-3"],$_POST["especialidades-medico-4"],$_POST["especialidades-medico-5"]);
+    /**Delete the 'especialidades' left blanc */
+    $especialidadesMedico = \array_diff($especialidadesMedico,["sin-especialidad"]);
 
-    $id_medico = $db->insert_id;
+
+    insertDoc($db, $nombreMedico, $regionMedico, $comunaMedico, $celMedico, 
+    $mailMedico, $twitMedico, $expMedico, $especialidadesMedico);
 }
 $db->close();
 
 
+function insertDoc($db, $nombre,$region, $comuna, $cel, $mail, $twit, $exp, $esp){
+    $encontrar_comuna="SELECT id FROM comuna WHERE nombre LIKE '$comuna'";
+    $resultado = $db->query($encontrar_comuna);
+    $id_comuna = mysqli_fetch_array($resultado)["id"];
+
+    $sql=$db->prepare("INSERT INTO medico (nombre, experiencia, comuna_id, twitter, email, celular) 
+    VALUES ('$nombre', '$exp', '$id_comuna', '$twit', '$mail', '$cel') ");
+    
+    $sql->execute();
+
+    $id_medico = $db->insert_id;
+    foreach ($esp as $especialidad){
+        if ($especialidad != NULL){
+            $encontrar_especialidad = "SELECT id FROM especialidad WHERE descripcion LIKE '$especialidad'";
+            $resultado = $db->query($encontrar_especialidad);
+            $id_especialidad = mysqli_fetch_array($resultado)["id"];
+
+            $sql2=$db->prepare("INSERT INTO especialidad_medico (medico_id,especialidad_id) VALUES ('$id_medico','$id_especialidad')");
+            $sql2->execute();
+        }
+    }
+}
 ?>
